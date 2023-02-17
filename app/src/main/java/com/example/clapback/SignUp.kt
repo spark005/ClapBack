3da53641256de4d20ca40c72596e3e1b9a49a8ce
LukeL25 <lawsonluke.business@gmail.com>
2023-02-17 15:13:25 -0500

    Luke's Prototype

---
+++ app/src/main/java/com/example/clapback/SignUp.kt
@@ -0,0 +1,77 @@
+package com.example.clapback
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class SignUp : AppCompatActivity() {
+
+
+    //TODO, figure out what lateinit var means in kotlin
+
+    // Text fields
+    private lateinit var editName: EditText
+    private lateinit var editEmail: EditText
+    private lateinit var editPassword: EditText
+    private lateinit var btnSignUp: Button
+
+    // Networking stuff
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        // Brief line to remove action bar
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
+
+        editName = findViewById(R.id.edit_name)
+        editEmail = findViewById(R.id.edit_email)
+        editPassword = findViewById(R.id.edit_password)
+        btnSignUp = findViewById(R.id.signup_button)
+
+        btnSignUp.setOnClickListener {
+            val name = editName.text.toString()
+            val email = editEmail.text.toString()
+            val password = editPassword.text.toString()
+
+            signUp(name, email,password)
+        }
+    }
+
+    //TODO, fix signup error "xxx "
+    // Function to signup
+    private fun signUp(name: String, email: String, password: String) {
+        // logic for creating a new user
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // If sign in is successful, jump to home
+                    addUserToDatabse(name, email, mAuth.currentUser?.uid!!)
+                    val intent = Intent(this@SignUp, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@SignUp, "Some error occurred", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
+    // Adding user to database
+    private fun addUserToDatabse(name: String, email: String, uid: String) {
+        mDbRef =FirebaseDatabase.getInstance().getReference()
+
+        mDbRef.child("user").child(uid).setValue(User(name, email, uid))
+    }
+}
