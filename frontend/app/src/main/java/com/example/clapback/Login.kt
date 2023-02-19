328adb6513605b581c7390fcdf8678b1862eaa27
Ilhoon Lee <ilhoonlee@Ilhoons-MacBook-Pro.local>
2023-02-18 22:28:45 -0500

    Added frontend

---
+++ frontend/app/src/main/java/com/example/clapback/Login.kt
@@ -0,0 +1,70 @@
+package com.example.clapback
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class Login : AppCompatActivity() {
+
+    //TODO, figure out what lateinit var means in kotlin
+    //Lateinit allows us to initialize a non-null variable outside constructor
+    private lateinit var editEmail: EditText
+    private lateinit var editPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: TextView
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Brief line to remove action bar
+        supportActionBar?.hide()
+
+        // Initializing firebase authentication
+        mAuth = FirebaseAuth.getInstance()
+
+        editEmail = findViewById(R.id.edit_email)
+        editPassword = findViewById(R.id.edit_password)
+        btnLogin = findViewById(R.id.login_button)
+        btnSignUp = findViewById(R.id.signup_link)
+
+        // Creating signup button functionality
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            val email = editEmail.text.toString()
+            val password = editPassword.text.toString()
+
+            login(email,password)
+        }
+    }
+
+    private fun login(email: String, password: String) {
+        // logic for logging in user
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, logs in user
+
+                    val intent = Intent(this@Login, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@Login, "User does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+}
