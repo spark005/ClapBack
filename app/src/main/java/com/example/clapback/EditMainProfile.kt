d337aa967dd9d4cf66f18d9a149f861184820c92
spark005 <park1343@purdue.edu>
2023-02-27 15:49:37 -0500

    pfp page and settings
    fixed profile page background sizing
    
    added settings page with connector to profile page

---
+++ app/src/main/java/com/example/clapback/EditMainProfile.kt
@@ -0,0 +1,14 @@
+package com.example.clapback
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GestureDetectorCompat
+
+class EditMainProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editmain_profile)
+
+    }
+}
