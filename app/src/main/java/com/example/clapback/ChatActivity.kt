4b5ae9a3bdba40d5d630584de0bacc449e69bdbe
ETBucky <buck46@purdue.edu>
2023-02-27 13:43:43 -0500

    It notifies the user that they themself sent a message

--- app/src/main/java/com/example/clapback/ChatActivity.kt
+++ app/src/main/java/com/example/clapback/ChatActivity.kt
@@ -1,10 +1,15 @@
 package com.example.clapback
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
+import androidx.core.app.NotificationCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
@@ -19,11 +24,16 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var messageAdapter: MessageAdapter
     private lateinit var messageList: ArrayList<Message>
     private lateinit var  mDbRef: DatabaseReference
+    private lateinit var channel: NotificationChannel
+    private lateinit var notificationManager: NotificationManager
 
     var receiverRoom: String? = null
     var senderRoom: String? = null
+    val CHANNEL_ID = "MESSAGE"
 
-
+    val name = "Hidden Messages"
+    val descriptionText = "You sent a message"
+    val importance = NotificationManager.IMPORTANCE_DEFAULT
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -77,6 +87,18 @@ class ChatActivity : AppCompatActivity() {
 
             })
 
+/* TODO Because you must create the notification channel before posting any notifications on Android 8.0 and higher,
+        you should execute this code as soon as your app starts.
+        It's safe to call this repeatedly because creating an existing notification channel performs no operation.*/
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+
+            notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
 
         //TODO using firebase database will have to change
         // adding the message to database
@@ -92,6 +114,15 @@ class ChatActivity : AppCompatActivity() {
                 }
             messageBox.setText("")
 
+
+
+            var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.logo)
+                .setContentTitle("YOU sent a message")
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            notificationManager.notify(1234, builder.build())
         }
     }
 }
