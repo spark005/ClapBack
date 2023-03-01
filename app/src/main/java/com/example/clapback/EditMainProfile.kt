3689a37b405a4c1a2a57fbc0c22a09fcd4b8bbed
ETBucky <buck46@purdue.edu>
2023-03-01 00:59:50 -0500

    Started working on edit profile pic

--- app/src/main/java/com/example/clapback/EditMainProfile.kt
+++ app/src/main/java/com/example/clapback/EditMainProfile.kt
@@ -1,6 +1,7 @@
 package com.example.clapback
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
@@ -8,6 +9,10 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.view.GestureDetectorCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+import de.hdodenhof.circleimageview.CircleImageView
+import java.io.File
 
 class EditMainProfile : AppCompatActivity() {
 
@@ -17,6 +22,7 @@ class EditMainProfile : AppCompatActivity() {
     private lateinit var name: EditText
     private lateinit var username: EditText
     private lateinit var bio: EditText
+    private lateinit var image: CircleImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editmain_profile)
@@ -30,6 +36,21 @@ class EditMainProfile : AppCompatActivity() {
         name = findViewById(R.id.name)
         username = findViewById(R.id.username)
         bio = findViewById(R.id.bio)
+        val profileUid = FirebaseAuth.getInstance().currentUser?.uid
+        val storage = FirebaseStorage.getInstance().reference.child("profilePic/$profileUid")
+
+        val pic = File.createTempFile("profile", "jpg")
+        storage.getFile(pic).addOnSuccessListener {
+            val bitmap = BitmapFactory.decodeFile(pic.absolutePath)
+            image.setImageBitmap(bitmap)
+
+        }.addOnFailureListener{
+            image.setImageResource(R.drawable.mongle)
+        }
+
+        changePic.setOnClickListener() {
+
+        }
 
         cancel.setOnClickListener() {
             val intent = Intent(this, ProfilePage::class.java)
