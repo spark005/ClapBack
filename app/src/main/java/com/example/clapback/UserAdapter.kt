81be57445f1bad5376674ed2cde17cca755d1b4f
ETBucky <buck46@purdue.edu>
2023-03-01 23:32:37 -0500

    Correctly orientated?

--- app/src/main/java/com/example/clapback/UserAdapter.kt
+++ app/src/main/java/com/example/clapback/UserAdapter.kt
@@ -2,12 +2,17 @@ package com.example.clapback
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.storage.FirebaseStorage
 import java.io.File
@@ -24,6 +29,7 @@ class UserAdapter (val context: Context, var userList: ArrayList<User>):
         return UserViewHolder(view)
     }
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
 
         val currentUser = userList[position]
@@ -33,7 +39,12 @@ class UserAdapter (val context: Context, var userList: ArrayList<User>):
         holder.textName.text = currentUser.name
         val pic = File.createTempFile("profile", "jpg")
         storage.getFile(pic).addOnSuccessListener {
-            val bitmap = BitmapFactory.decodeFile(pic.absolutePath)
+            val bitmap: Bitmap =
+                modifyOrientation(
+                    BitmapFactory.decodeFile(pic.absolutePath),
+                    pic.absolutePath
+                )
+
             holder.image.setImageBitmap(bitmap)
 
         }.addOnFailureListener{
@@ -70,4 +81,48 @@ class UserAdapter (val context: Context, var userList: ArrayList<User>):
     fun getFilteredList(): ArrayList<User> {
         return filteredList
     }
+
+    private fun modifyOrientation(bitmap: Bitmap, image_absolute_path: String): Bitmap {
+        val ei: ExifInterface = ExifInterface(image_absolute_path);
+        val orientation: Int =
+            ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+        when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> {
+                return rotate(bitmap, 90f)
+            }
+            ExifInterface.ORIENTATION_ROTATE_180 -> {
+                return rotate(bitmap, 180f)
+            }
+            ExifInterface.ORIENTATION_TRANSVERSE -> {
+                return rotate(bitmap, 270f)
+            }
+            ExifInterface.ORIENTATION_ROTATE_270 -> {
+                return rotate(bitmap, 270f)
+            }
+            ExifInterface.ORIENTATION_FLIP_HORIZONTAL -> {
+                return flip(bitmap, true, vertical = false)
+            }
+            ExifInterface.ORIENTATION_FLIP_VERTICAL -> {
+                return flip(bitmap, false, vertical = true)
+            }
+            else -> {
+                return bitmap
+            }
+        }
+    }
+
+    private fun rotate(bitmap: Bitmap, degrees: Float): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(degrees)
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+    }
+
+    private fun flip(bitmap: Bitmap, horizontal: Boolean, vertical: Boolean): Bitmap {
+        val matrix = Matrix()
+        matrix.preScale(if (horizontal) (-1f) else 1f, if (vertical) (-1f) else 1f)
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true);
+    }
+
 }
+
