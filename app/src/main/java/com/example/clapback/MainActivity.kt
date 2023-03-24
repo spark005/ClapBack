bb79da3e859be43c687466315effe46c9829abbe
chowder985 <ilhoonlee1@gmail.com>
2023-03-23 23:53:58 -0400

    Fix swipe animation

--- app/src/main/java/com/example/clapback/MainActivity.kt
+++ app/src/main/java/com/example/clapback/MainActivity.kt
@@ -213,8 +213,8 @@ class MainActivity : AppCompatActivity(), OnSwipeListener {
     override fun onSwipeLeft() {
         val intent = Intent(this, Time::class.java)
         startActivity(intent)
-        this.overridePendingTransition(R.anim.swipe_screen_left,
-            R.anim.swipe_screen_right)
+        this.overridePendingTransition(R.anim.swipe_screen_right2,
+            R.anim.swipe_screen_left2)
     }
 
     override fun finish() {
