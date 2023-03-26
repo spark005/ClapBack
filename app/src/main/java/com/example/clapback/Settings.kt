44366161679d25e192579203e361ebd72946200b
LukeL25 <lawsonluke.business@gmail.com>
2023-03-26 02:32:04 -0400

    Added functional settings page

---
+++ app/src/main/java/com/example/clapback/Settings.kt
@@ -0,0 +1,13 @@
+package com.example.clapback
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Settings : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.setting_layout)
+    }
+
+}
