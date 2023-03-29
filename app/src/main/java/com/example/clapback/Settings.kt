3cdf7502de8e78cfac0be51041081a954952b767
LukeL25 <lawsonluke.business@gmail.com>
2023-03-29 02:12:15 -0400

    Added user deletion functionality and fixed edit profile bug

--- app/src/main/java/com/example/clapback/Settings.kt
+++ app/src/main/java/com/example/clapback/Settings.kt
@@ -1,9 +1,17 @@
 package com.example.clapback
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
 
 class Settings : AppCompatActivity() {
 
@@ -11,9 +19,17 @@ class Settings : AppCompatActivity() {
     private lateinit var changeEmail: Button
     private lateinit var delete : Button
     private lateinit var back : Button
+    private lateinit var mDbRef: DatabaseReference
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.setting_layout)
+
+        // Establishing user stuff for account deletion
+        val user = Firebase.auth.currentUser!!
+        mDbRef = FirebaseDatabase.getInstance().getReference()
+        mAuth = FirebaseAuth.getInstance()
 
         changePass = findViewById(R.id.change_password_button)
         changeEmail = findViewById(R.id.change_email_button)
@@ -31,13 +47,75 @@ class Settings : AppCompatActivity() {
         }
 
         delete.setOnClickListener {
-            val intent = Intent(this, DeleteAccount::class.java)
-            startActivity(intent)
+            val warning = AlertDialog.Builder(this)
+            warning.setTitle("WARNING: ACCOUNT DELETION")
+            warning.setMessage("Are you sure you want to delete your account?")
+
+            /* TODO LUKE, you didn't *actually* delete the user from each other their
+           friend's friendlists. You gotta do that. Right now it actually just
+           won't display the user in their chat history */
+
+
+            // Warning popup if yes
+            warning.setPositiveButton("Yes") { dialog, which ->
+                deleteUser(user)
+                val intent = Intent(this, Login::class.java)
+                startActivity(intent)
+            }
+
+            // Warning popup if no
+            warning.setNegativeButton("No") { dialog, which ->
+                return@setNegativeButton
+            }
+            warning.show()
         }
 
         back.setOnClickListener {
-            finish()
+            val intent = Intent(this, ProfilePage::class.java)
+            startActivity(intent)
         }
+    }
+
+
+    // Function to completely delete user
+    private fun deleteUser(user: FirebaseUser) {
+        mDbRef.child("user").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                for (postSnapshot in snapshot.children) {
+                    val foundFriend = postSnapshot.getValue(User::class.java)
+
+                    if (foundFriend?.uid.equals(user.uid)) {
+                        postSnapshot.ref.removeValue()
+
+                        user.delete()
+                            .addOnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    Log.d("This is a Tag", "User account deleted.")
+                                } else {
+                                    Toast.makeText(
+                                        this@Settings,
+                                        "ERROR: Account couldn't be deleted!",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+
+                        // Signing out user
+                        mAuth.signOut()
+
+
+                        break
+                    }
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
     }
 
 }
