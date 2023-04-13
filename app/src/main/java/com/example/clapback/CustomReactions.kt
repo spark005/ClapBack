5252f4590095a851ad36c68fff6b2439fbe79139
ETBucky <buck46@purdue.edu>
2023-04-13 16:04:04 -0400

    Add saving the reaction to storage

--- app/src/main/java/com/example/clapback/CustomReactions.kt
+++ app/src/main/java/com/example/clapback/CustomReactions.kt
@@ -1,20 +1,27 @@
 package com.example.clapback
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.storage.FirebaseStorage
+
 
 class CustomReactions : AppCompatActivity() {
 
     private lateinit var add: Button
+    private lateinit var save: Button
     private lateinit var newReaction: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +29,8 @@ class CustomReactions : AppCompatActivity() {
         setContentView(R.layout.activity_custom_reactions)
 
         add = findViewById(R.id.addButton)
+        save = findViewById(R.id.saveButton)
+        val profileUid = FirebaseAuth.getInstance().currentUser?.uid
 
         //FUNCTION FOR WHEN YOU SUCCESSFULLY GRAB AN IMAGE
         val getPic = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
@@ -41,8 +50,11 @@ class CustomReactions : AppCompatActivity() {
                 reactionS.setImageURI(result.data?.data)
 
                 newReaction = result.data?.data!!
+
+                save.visibility = View.VISIBLE
             }
         }
+
 
         add.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK)
@@ -52,6 +64,34 @@ class CustomReactions : AppCompatActivity() {
             intent.action = Intent.ACTION_GET_CONTENT
             getPic.launch(intent)
 
+        }
+
+        save.setOnClickListener {
+            //Name dialoge
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setTitle("Title")
+
+            val input = EditText(this)
+            builder.setView(input)
+
+            //Save
+            builder.setPositiveButton("OK"){ dialog, which ->
+                if (input.text.isNotEmpty()) {
+                    val name = input.text.toString()
+
+                    val store = FirebaseStorage.getInstance().getReference("reactions/$profileUid/$name")
+
+                    store.putFile(newReaction)
+                    dialog.cancel()
+                    save.visibility = View.GONE
+                }
+            }
+
+            builder.setNegativeButton("Cancel"){ dialog, which ->
+                dialog.cancel()
+            }
+
+            builder.show()
         }
     }
 
